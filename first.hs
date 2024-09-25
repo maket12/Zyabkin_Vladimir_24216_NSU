08d3f3b8daf674079d2072f0d56e95fece3ee701
maket12 <150025985+maket12@users.noreply.github.com>
2024-09-25 22:03:24 +0700

    Add files via upload

--- first.hs
+++ first.hs
@@ -1,12 +1,11 @@
-f :: Float -> Float
-f x = x * 2
-g :: Float -> Float
-g x = x * 3
+f :: Double -> Double
+f x = (x + 2) * 8
 
 
 
-func :: (Float -> Float) -> (Float -> Float) -> Float -> Float
-func f g p = max (f p) (g p)
+func :: (Double -> Double) -> Double -> Double
+func f p = max (exp p) (f p) 
 
--- func f g 10  --> 30.0 (it means that g - max)
--- func f g 17  --> 51.0 (it means the same ^^^)
+-- func f 10  --> 22026.46579.... (it means that exp(p) returns max value)
+-- func f 1  --> 24.0 (it means that f(p) returns max value)
+-- func f 3  --> 40.0 (it means the same ^^^^)
