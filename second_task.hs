73976c62d7a63bd6e63ab9c13456697327c64c9f
maket12 <150025985+maket12@users.noreply.github.com>
2024-10-02 11:25:18 +0700

    Delete second_task.hs

--- second_task.hs
+++
@@ -1,9 +0,0 @@
-f :: Double -> Double
-f x = (x + 2) * 8
-
-func :: (Double -> Double) -> Double -> Double
-func f p = max (exp p) (f p) 
-
--- func f 10  --> 22026.46579.... (it means that exp(p) returns max value)
--- func f 1  --> 24.0 (it means that f(p) returns max value)
--- func f 3  --> 40.0 (it means the same ^^^^)
