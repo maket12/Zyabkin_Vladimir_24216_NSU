c61f4824845700714c1dbde4efc5e16be4590043
maket12 <150025985+maket12@users.noreply.github.com>
2024-10-02 11:22:21 +0700

    first_task.hs

--- first_task.hs
+++ first_task.hs
@@ -10,3 +10,13 @@ func f g p = max (f p) (g p)
 
 -- func f g 10  --> 30.0 (it means that g - max)
 -- func f g 17  --> 51.0 (it means the same ^^^)
+
+f :: Double -> Double
+f x = (x + 2) * 8
+
+func :: (Double -> Double) -> Double -> Double
+func f p = max (exp p) (f p) 
+
+-- func f 10  --> 22026.46579.... (it means that exp(p) returns max value)
+-- func f 1  --> 24.0 (it means that f(p) returns max value)
+-- func f 3  --> 40.0 (it means the same ^^^^)
