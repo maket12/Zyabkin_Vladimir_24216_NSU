2d684e70292e4d83c4c41bd49c39c31369b5e24e
Zyabkin Vladimir Vladimirovich <150025985+maket12@users.noreply.github.com>
2024-10-31 09:20:18 +0700

    Update first_task.hs.
    Update first_task.hs. Edited second problem, solve using func from first problem

--- first_task.hs
+++ first_task.hs
@@ -5,21 +5,21 @@ g x = x * 3
 
 
 
-func :: (Float -> Float) -> (Float -> Float) -> Float -> Float
+func :: (Double -> Double) -> (Double -> Double) -> Double -> Double
 func f g p = max (f p) (g p)
 
 -- func f g 10  --> 30.0 (it means that g - max)
 -- func f g 17  --> 51.0 (it means the same ^^^)
 
-f :: Double -> Double
-f x = (x + 2) * 8
+foo :: Double -> Double
+foo x = (x + 2) * 8
 
-func :: (Double -> Double) -> Double -> Double
-func f p = max (exp p) (f p) 
+some :: (Double -> Double) -> Double -> Double
+some f p = func exp f p
 
--- func f 10  --> 22026.46579.... (it means that exp(p) returns max value)
--- func f 1  --> 24.0 (it means that f(p) returns max value)
--- func f 3  --> 40.0 (it means the same ^^^^)
+-- some foo 10  --> 22026.46579.... (it means that exp(p) returns max value)
+-- some foo 2  --> 32.0 (it means that foo(p) returns max value)
+-- some foo 3  --> 40.0 (it means the same ^^^^)
 
 f :: Double -> Double
 f x = x + 3 + cos (pi/3)
